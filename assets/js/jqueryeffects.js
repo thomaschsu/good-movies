378904277cdba7af7d312d6c97aaedd4687f3622
Tom Hsu <thomaschsu@gmail.com>
2018-07-01 15:30:51 -0400

    Modified jquery

---
+++ assets/js/jqueryeffects.js
@@ -0,0 +1,8 @@
+$(function(){
+    // $("#alert-msg").hide().fadeIn(2000);
+    var navbar = $(".navbar-brand");
+    navbar.animate({fontSize: '30px'}, "slow");
+    $("#main-container").hide().fadeIn(2000);
+    $(".overlay-banner").hide().fadeIn(1000);
+    $(".navbar-nav").hide().fadeIn(1000);
+ });
